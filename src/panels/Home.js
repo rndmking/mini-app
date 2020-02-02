a941b057bce6252c12b9cc4d18abc02750d557b6
Robert <zombchannelru@gmail.com>
2020-02-02 15:54:49 +0300

    Update Home.js

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -10,7 +10,7 @@ import Avatar from '@vkontakte/vkui/dist/components/Avatar/Avatar';
 
 const Home = ({ id, go, fetchedUser }) => (
 	<Panel id={id}>
-		<PanelHeader>ПРИВЕТ МИР</PanelHeader>
+		<PanelHeader>ЗДАРОВА ДРУГ</PanelHeader>
 		{fetchedUser &&
 		<Group title="User Data Fetched with VK Connect">
 			<Cell
